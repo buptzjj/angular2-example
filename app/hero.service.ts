d9276c073dcad15b8a4e29434a008af0ed750b93
butpzjj <zhaojj659672365@163.com>
2016-12-01 22:31:55 +0800

    local update file

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Headers,Http} from '@angular/http';
 
+import 'rxjs/add/operator/toPromise';
 import { Hero } from './hero';
 import {HEROES} from './mock-heroes';
 
+
 @Injectable()
 export class HeroService{
+    private heroUrl = 'app/heroes';
+    private handleError(error:any):Promise<any> {
+        console.error('An error occurs',error);
+        return Promise.reject(error.message || error);
+    }
+    constructor(private http:Http){}
+
     getHeroes():Promise<Hero[]>{
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroUrl)
+        .toPromise()
+        .then(response => response.json().data as Hero[])
+        .catch(this.handleError);
     }
     getHeroesSlowly():Promise<Hero[]>{
         return new Promise<Hero[]>(resolve=>setTimeout(resolve,2000))
